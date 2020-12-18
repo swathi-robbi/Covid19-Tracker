2ec8c8136691fbb0f87db7801921d99b6b9f8ff8
swathi-robbi <64188901+swathi-robbi@users.noreply.github.com>
2020-12-18 17:29:04 -0500

    Create Chart.jsx

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,38 @@
+ import React from 'react';
+import { Bar } from 'react-chartjs-2';
+
+import { fetchDailyData } from '../../api';
+
+import styles from './Chart.module.css';
+
+const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
+
+
+  const barChart = (
+    confirmed ? (
+      <Bar
+        data={{
+          labels: ['Infected', 'Recovered', 'Deaths'],
+          datasets: [
+            {
+              label: 'People',
+              backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)'],
+              data: [confirmed.value, recovered.value, deaths.value],
+            },
+          ],
+        }}
+        options={{
+          legend: { display: false },
+        }}
+      />
+    ) : null
+  );
+
+  return (
+    <div className={styles.container}>
+      {barChart}
+    </div>
+  );
+};
+
+export default Chart;
